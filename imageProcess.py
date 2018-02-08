c6b2340c3327119d1a8d340128f386da35e73d95
Moon Junoh <mjo970625@gmail.com>
2018-02-08 13:41:26 +0900

    픽셀 카운트 초안

--- imageProcess.py
+++ imageProcess.py
@@ -1,15 +1,21 @@
 from PIL import Image
+from itertools import chain
+import numpy as np
 
 
-def convert_to_binary(name: str, threshold=90):
+def convert_to_bw(name: str, threshold=90):
 	img: Image.Image = Image.open(name)
 	grey = img.convert('L')
 	bw = grey.point(lambda x: 255 if x > threshold else 0)
 	return bw
 
-def showImage(img: Image.Image):
-	img.show()
+def count_if(iterable, func):
+    return sum(1 for x in iterable if func(x))
+
+def count_pixels(img: Image.Image):
+    matrix = np.asarray(img)
+    ary = list(chain(*matrix))
+    return count_if(ary, lambda x: x == 0), len(ary)
 
 if __name__ == "__main__":
-	ret = convert_to_binary("lena_std.tif")
-	showImage(ret)
+	ret = convert_to_bw("lena_std.tif")
