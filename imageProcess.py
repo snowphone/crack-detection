9995ae6bc3e7d307554aab7bc878fdbc320e108a
Moon Junoh <mjo970625@gmail.com>
2018-02-08 15:28:59 +0900

    코드 정리

--- imageProcess.py
+++ imageProcess.py
@@ -1,21 +1,30 @@
-from PIL import Image
 from itertools import chain
+
 import numpy as np
+from PIL import Image
+
+BLACK = 0
+WHITE = 255
 
 
 def convert_to_bw(name: str, threshold=90):
 	img: Image.Image = Image.open(name)
 	grey = img.convert('L')
-	bw = grey.point(lambda x: 255 if x > threshold else 0)
+	bw = grey.point(lambda x: WHITE if x > threshold else BLACK)
 	return bw
 
+
 def count_if(iterable, func):
-    return sum(1 for x in iterable if func(x))
+	return sum(1 for x in iterable if func(x))
+
 
 def count_pixels(img: Image.Image):
-    matrix = np.asarray(img)
-    ary = list(chain(*matrix))
-    return count_if(ary, lambda x: x == 0), len(ary)
+	matrix = np.asarray(img)
+	ary = list(chain(*matrix))
+	return count_if(ary, lambda x: x == BLACK), len(ary)
+
 
 if __name__ == "__main__":
-	ret = convert_to_bw("lena_std.tif")
+	name = "images/crack.jpg"
+	ret = convert_to_bw(name)
+	a = count_pixels(ret)
