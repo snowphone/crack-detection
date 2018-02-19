3ebee09aafd7da922465c61d68e4e452d18ccd1d
Moon Junoh <mjo970625@gmail.com>
2018-02-19 19:01:22 +0900

    one-hot 벡터 버전의 함수 추가

--- labeler.py
+++ labeler.py
@@ -3,3 +3,15 @@ import re
 def label(nameList):
 	finder = re.compile(r"시작|초기|중기|말기|최종")
 	return [finder.search(name).group() for name in nameList]
+
+def label_onehot(nameList):
+	classifier = ["시작", "초기", "중기", "말기", "최종"]
+	finder = re.compile(r"시작|초기|중기|말기|최종")
+	ret = []
+	for name in nameList:
+		onehot = [0 for _ in range(len(classifier))]
+		idx = classifier.index(finder.search(name))
+		onehot[idx] = 1
+		ret.append(onehot)
+	return ret
+		
